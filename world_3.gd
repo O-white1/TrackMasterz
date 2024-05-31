e7508966fb7be993af0376dac1c8e1223c7a1e05
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-05-31 15:24:58 -0500

    TrackMasterz

--- world_3.gd
+++ world_3.gd
@@ -4,7 +4,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# $AudioStreamPlayer.play("res://Assets/Music/Stage 4 Theme_.mp3")
- 	pass
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
