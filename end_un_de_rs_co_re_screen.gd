27b9939cd6eed07bf43024046f0cdcf770b7b2f9
Owen White <white.o3@stu.janesville.k12.wi.us>
2024-06-06 15:26:57 -0500

    june 6

--- end_un_de_rs_co_re_screen.gd
+++ end_un_de_rs_co_re_screen.gd
@@ -1,10 +1,12 @@
 extends Control
 
-
-
 func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://world_1.tscn")
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
